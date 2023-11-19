34193ca2c5626f9be0ef28d59a95c7e707f1971d
yuzuki0127 <funamoto112987@gmail.com>
2023-11-19 09:27:45 +0900

    完成

--- js/rihito.js
+++ js/rihito.js
@@ -6,18 +6,10 @@ const slideOptions = {
   width: '1096px',
   perPage: 3,
   padding: '5rem',
-  
   pagination: false,
-  breakpoints: {
-    768: {
-      pagination: true,
-      perPage: 1,
-    }
-  }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-
   const slider = new Splide( '.splide', slideOptions
   ).mount();
 });
--- js/yuzuki.js
+++ js/yuzuki.js
@@ -3,7 +3,6 @@ window.addEventListener("scroll" , function () {
     const scroll = Array.from(document.querySelectorAll(".scroll-up"));
     scroll.forEach(element => {
         const distance = element.offsetTop;
-        //elementの上端が、親要素の上端からどれだけ離れているかをで取得
         const windowHeight = window.innerHeight;
         if (window.scrollY > distance - windowHeight + 200) {
             element.classList.add('on');
